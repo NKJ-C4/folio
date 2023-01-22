094ab07924360af9ea1aedd13dda85dd331be538
Nishant <nkj.nishant@gmail.com>
2023-01-23 01:06:00 +0530

    boilerplate ready, work, contact and skills remaining

---
+++ src/resumeData.js
@@ -0,0 +1,103 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Nishant Kumar Jha",
+    "role": "Frontend Developer and an IT Engineer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to explore frontend development, learn about new technologies, write technical articles or simply play games and vibe to music in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/nkjnishant/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/rbhatia46",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"https://www.instagram.com/nkjnishant/",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"Having done my graduation in B.Tech (Information technology) from Gautam Buddha University in 2022, I am currently working as an SDE - 1 (Frontend) at Micro Focus. I am a self taught Web Developer, currently learning backend web development using flask, looking forward to become a full stack web developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them. A \'learning-by-doing\' methodology is what drives me.",
+    "address":"Greater Noida, India",
+    "website":"https://nkj-c4.github.io",
+    "education":[
+      {
+        "UniversityName":"Gautam Buddha University, School of ICT",
+        "specialization":"Information Technology",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2022",
+        "Achievements":"Secured 8.1 CGPA"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Micro Focus",
+        "specialization":"Software Engineer (Frontend)",
+        "MonthOfJoining": "Oct",
+        "YearOfJoining": "2022",
+        "MonthOfLeaving": "Present",
+        "YearOfLeaving": "",
+        "Achievements": "Working on a project based on a platform for Big data and Machine Learning - Vertica Accelerator. Collaborating with a huge team of developers. Implementing SAFe framework in development process."
+      },
+      {
+        "CompanyName":"Navia Life Care",
+        "specialization":"Frontend Developer",
+        "MonthOfJoining": "Jan",
+        "YearOfJoining": "2022",
+        "MonthOfLeaving":"September",
+        "YearOfLeaving":"2022",
+        "Achievements":"Developed responsive UI using React JS. Developed finance app Aarogya Pay and a user dashboard for Aarogya Pay which included several graphs, implemented using Recharts library."
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
